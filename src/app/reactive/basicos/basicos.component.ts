c52e013abbf044740235591a6551cb7503cd6cf3
JanPLosa <jplosa@tlsi.es>
2022-04-22 16:25:46 +0200

    FormBuilder

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -9,10 +9,18 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class BasicosComponent {
 
-  miFormulario: FormGroup = new FormGroup({
-    'nombre': new FormControl('Wolo')
+  //miFormulario: FormGroup = new FormGroup({
+  //  'nombre'     : new FormControl('Wolo'),
+  //  'precio'     : new FormControl( 10 ),
+  //  'existencias': new FormControl( 5 ),
+  //})
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre      : [ 'Wolo' ],
+    precio      : [ 0 ],
+    existencias : [ 0 ]
   })
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
 
 }
