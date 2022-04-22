312bb5ac408407f39c95aab00cceaa9b7d0fa66b
JanPLosa <jplosa@tlsi.es>
2022-04-22 16:33:25 +0200

    Validaciones básicas

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -16,9 +16,9 @@ export class BasicosComponent {
   //})
 
   miFormulario: FormGroup = this.fb.group({
-    nombre      : [ 'Wolo' ],
-    precio      : [ 0 ],
-    existencias : [ 0 ]
+    nombre      : [ 'Wolo', [ Validators.required, Validators.minLength(3) ] ],
+    precio      : [ 0, [ Validators.min(0), Validators.required ]  ],
+    existencias : [ 0, [ Validators.min(0), Validators.required ]  ]
   })
 
   constructor( private fb: FormBuilder) { }
